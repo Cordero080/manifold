86bc20c45d1d8a6137d5987f211218e5dc9dbffa
Pablo Cordero <cordero080@gmail.com>
2026-02-15 00:16:13 -0500

    chore: remove dead code
    - Delete unused threeConstants.js
    - Delete duplicate portalWorlds.js (src/data/ version is used)
    - Delete unused CustomCursor component

--- src/components/ui/Effects/index.js
+++ src/components/ui/Effects/index.js
@@ -1,7 +1,6 @@
 /**
  * Effects components - Visual effects and enhancements for the UI/3D scene
  */
-import CustomCursor from './CustomCursor/CustomCursor';
 import QuantumCursor from './QuantumCursor/QuantumCursor';
 
-export { CustomCursor, QuantumCursor };
+export { QuantumCursor };
